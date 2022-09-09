96dca740c37967a52a361188eda914726e0312f2
LucasMarzochi <111364839+LucasMarzochi@users.noreply.github.com>
2022-09-09 09:17:58 -0300

    Add files via upload

---
+++ tabela02.sql
@@ -0,0 +1,15 @@
+
+create table tabela02(
+
+nome varchar(100),
+
+ra int,
+
+professor varchar(100),
+
+materia varchar (100)
+);
+
+insert into tabela02 (nome, ra, professor, materia)
+
+values (Lucas, 22009005, xastre, matematica)
