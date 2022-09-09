b01a50fd62e217afa62300c1a9500397534d86ff
GuilhermeLopes1003 <112004721+GuilhermeLopes1003@users.noreply.github.com>
2022-09-09 09:26:30 -0300

    Add files via upload

---
+++ Tabela3.sql
@@ -0,0 +1,19 @@
+CREATE TABLE tabela3 (
+ ra INT,
+ nome VARCHAR(100),
+nascimento DATE
+ );
+  INSERT INTO tabela3 (ra, nome , nascimento)
+  VALUES ( 22019085, "Guilherme", "2004-03-10");
+  
+  INSERT INTO tabela3 (ra, nome, nascimento)
+  VALUES (22075674, "Pedro", "2004-05-21");
+  
+  INSERT INTO tabela3 (ra, nome, nascimento)
+  VALUES (22098765, "Lucas", "2004-07-29");
+  
+  INSERT INTO tabela3 (ra, nome, nascimento)
+  VALUES (22018975, "Vinicius", "2004-12-23");
+  
+  INSERT INTO tabela3 (ra, nome, nascimento)
+  VALUES (22098765, "Gabriel", "2003-08-25");
